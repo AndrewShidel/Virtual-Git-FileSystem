5d7da6d1893fbac8dae15f5fdd5257656d7a1eeb
AndrewShidel <andrew@shidel.com>
2019-11-17 19:33:45 -0800

    Implements basic functionality (still not working) and adds tests.

--- src/git.rs
+++ src/git.rs
@@ -1,17 +1,50 @@
+extern crate tempdir;
 
 use git2::build::{RepoBuilder};
 use std::path::{Path};
+use tempdir::TempDir;
 
 pub fn clone_if_not_exist(repo_path: &String, cache_dir: String) -> Result<String, std::io::Error> {
     let parts: Vec<&str> = repo_path.split("/").collect();
     if parts.len() < 3 {
-        // TODO(ashidel): Return error
-        return Ok("".to_string())
+        // TODO: Return error
+        return Ok("/".to_string())
     }
-    let url = "https://".to_owned() + parts[0..2].join("/").as_str() + ".git";
-    debug!("Final URL: {:?}", url);
-    match RepoBuilder::new().clone(&url, Path::new(&cache_dir)) {
-        Ok(_r) => Ok(cache_dir),
+    let url = "https://".to_owned() + parts[0..3].join("/").as_str() + ".git";
+    let real_repo_path = format!("{}/{}", cache_dir, parts[2]);
+    println!("Final URL: {:?}", url);
+    // TODO: Check if it exists before cloning.
+    match RepoBuilder::new().clone(&url, Path::new(&real_repo_path)) {
+        Ok(_r) => Ok(format!("{}/{}", cache_dir, parts[2..].join("/").as_str())),
         Err(e) => Err(std::io::Error::new(std::io::ErrorKind::NotFound, e))
+    }
+}
+
+//TODO: Mock out clone step for tests.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_clone_root() -> Result<(), std::io::Error> {
+        let path = clone_if_not_exist(&"".to_string(), "/tmp/cache".to_string())?;
+        assert_eq!(path, "/");
+        Ok(())
+    }
+
+    #[test]
+    fn test_clone_full() -> Result<(), std::io::Error> {
+        let tmp_dir = TempDir::new("cache")?;
+        let path = clone_if_not_exist(&"github.com/AndrewShidel/Resume".to_string(), tmp_dir.path().to_str().unwrap().to_string())?;
+        assert_eq!(path, tmp_dir.path().join("Resume").to_str().unwrap());
+        Ok(())
+    }
+
+    #[test]
+    fn test_clone_extras() -> Result<(), std::io::Error> {
+        let tmp_dir = TempDir::new("cache")?;
+        let path = clone_if_not_exist(&"github.com/AndrewShidel/Resume/resume.tex".to_string(), tmp_dir.path().to_str().unwrap().to_string())?;
+        assert_eq!(path, format!("{}/Resume/resume.tex", tmp_dir.path().to_str().unwrap()));
+        Ok(())
     }
 }
